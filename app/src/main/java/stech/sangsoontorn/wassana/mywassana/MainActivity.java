f5fd2322a5d882479da38df0c2d232789e873193
wassanasang <wassanasang@stech.ac.th>
2017-04-04 16:01:44 +0700

    Before Lunch

--- app/src/main/java/stech/sangsoontorn/wassana/mywassana/MainActivity.java
+++ app/src/main/java/stech/sangsoontorn/wassana/mywassana/MainActivity.java
@@ -2,12 +2,33 @@ package stech.sangsoontorn.wassana.mywassana;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //  Explicit เพื่อประกาศตัวแปร
+    //  Access(public private) Type ผint str dub bul)   Name
+    private EditText userEditText,passwordEditText;
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initial View คือการผูกตัวแหร กับ View ที่ อยู่ที่ Activity
+        initialView();
+
+
+    }   //Main Method
+
+    private void initialView() {
+        userEditText= (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtRegister);
+        button = (Button) findViewById(R.id.btnLogin);
     }
-}
+}   //Main Class นี่คือ คลาสหลัก
